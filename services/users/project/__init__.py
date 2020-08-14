cedddf265a56c7163f525ce4428b0fac5d38ff94
Mike Hartington <mikehartington@gmail.com>
2020-08-13 20:27:45 -0500

    add test

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,9 @@
 # services/users/project/__init__.py
 
-import sys
 import os  # new
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -16,6 +16,22 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')  # new
 app.config.from_object(app_settings)  # new
 
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.uername = username
+        self.email = email
+
 
 class UsersPing(Resource):
     def get(self):
@@ -26,4 +42,3 @@ class UsersPing(Resource):
 
 
 api.add_resource(UsersPing, '/users/ping')
-print(app.config, file=sys.stderr)
